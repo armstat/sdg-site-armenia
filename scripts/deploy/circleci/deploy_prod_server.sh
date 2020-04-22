476e10217b2784503db5c082fc3b5d9a90ef6432
SDGArmenia <46884106+SDGArmenia@users.noreply.github.com>
2020-04-22 14:25:59 +0400

    Update deploy_prod_server.sh

--- scripts/deploy/circleci/deploy_prod_server.sh
+++ scripts/deploy/circleci/deploy_prod_server.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # This script deploys the built site to the server.
 
-SERVER="91.221.229.124"
-USERNAME="rubo"
-FOLDER="/var/www/html/"
 
 # The fully built site is already available at ~/repo/_site. Transfer it using
 # rsync.
